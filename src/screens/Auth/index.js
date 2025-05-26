819a062d0c149a92da549ac72d35e89e51d05d10
cerff-gur-sheel <ayslan.gamedev@gmail.com>
2025-05-26 02:59:47 -0300

    Refactor authentication screens: consolidate Login and Register components into Auth screen, update UI elements, and remove unused files.

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -1,22 +1,25 @@
 // src/screens/Auth/RegisterScreen.js
-import { View } from "react-native";
 import { useEffect, useState } from "react";
-import { BackHandler } from "react-native";
+import { View, BackHandler, Text, Button } from "react-native";
+import { InputContainer, ButtonContainer } from '../../Components/Containers';
+import { Container, DivisionText, Image } from './styles';
 
+import logo from '../../../assets/biababylogo.jpg';
+import avatar from '../../../assets/avatar.png'
+import google from '../../../assets/google.png'
+import { colors } from '../../styles/styles';
 
-import HomeAuth from "./home.js";
-import RegisterScreen from "./register.js";
-import LoginScreen from "./login.js";
 
 export default function Auth() {
-  const [activeScreen, setActiveScreen] = useState("login"); // "auth", "login", "register"
+  const [activeScreen, setActiveScreen] = useState("login"); // "login", "register"
   useEffect(() => {
     const backAction = () => {
-      if (activeScreen !== "auth") {
-        setActiveScreen("auth");
-        return true;
-      }
-      return false;
+      alert("Pressione novamente para sair");
+      // if (activeScreen !== "auth") {
+      //   setActiveScreen("login");
+      //   return true;
+      // }
+      // return false;auth
     };
 
     const backHandler = BackHandler.addEventListener(
@@ -29,15 +32,41 @@ export default function Auth() {
 
   return (
     <View>
-      {activeScreen === "auth" && (
-        <HomeAuth
-          onLoginClick={() => setActiveScreen("login")}
-          onRegisterClick={() => setActiveScreen("register")}
-        />
-      )}
       {activeScreen === "login" && <LoginScreen />}
       {activeScreen === "register" && <RegisterScreen />}
       
     </View>
+  );
+}
+
+
+function LoginScreen() {
+  return (
+    <Container style={{ backgroundColor: colors.screen.auth.login.background }}>
+      <Image source={logo} />
+      <InputContainer image={avatar} text='E-mail' textColor={colors.screen.auth.login.text} />
+      <InputContainer image={avatar} text='Senha' textColor={colors.screen.auth.login.text} />
+      <ButtonContainer text='ENTRAR' backgroundColor={colors.button.confirm.background} style={{ Text: { color: colors.button.confirm.text } }} />
+      <DivisionText>---------------- ou ----------------- </DivisionText>
+      <ButtonContainer image={google} text='Entrar com Google' borderColor={colors.button.google.text} />
+   </Container>
+  );
+}
+
+
+function RegisterScreen() {
+  return (
+    <Container>
+      <Text>Register</Text>
+
+      <Input placeholder="Nome" />
+      <Input placeholder="Sobrenome" />
+      <Input placeholder="Email" />
+      <Input placeholder="Senha" />
+      <Input placeholder="Confirmar Senha" />
+
+      <Button title="Cadastrar" />
+      <Button title="Continuar com Google" />
+    </Container>
   );
 }
