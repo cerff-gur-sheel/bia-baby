29af87ebcb3d93a3c0830e241ac74160e56592ce
cerff-gur-sheel <ayslan.gamedev@gmail.com>
2025-05-29 14:25:29 -0300

    Refactor Footer component: remove unused screenHeight variable and clean up styling

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components/native';
 import { Platform, Dimensions } from 'react-native';
 
-const screenHeight = Dimensions.get('window').height;
-
 const extraBottom = Platform.OS === 'android' ? 24 : 0;
 
 const Container = styled.View`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
   height: 56px;
   padding-bottom: ${extraBottom}px;
   background-color: ${({ bg }) => bg || '#000'};
